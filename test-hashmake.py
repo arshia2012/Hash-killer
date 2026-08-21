04f4aa22fa3e40e10435ebf833515af7211c6e2e
aCoDeR <rahbari.arshia@gmail.com>
2026-08-21 12:38:38 -0400

    Update test-hashmake.py

--- test-hashmake.py
+++ test-hashmake.py
@@ -3,10 +3,11 @@ from cryptography.hazmat.backends import default_backend
 
 
 digest = hashes.Hash(
-    hashes.MD5(),
+    hashes.MD5(), #change the algorithm if you wanted to get other types of hashed text
     backend=default_backend
 )
 
+print("Write your text down")
 text = input()
 
 digest.update(text.encode())
